74c6addb0a59f8f717dc53bc36f9c9a57524ee7e
Jonathan Moallem <jdmoallem@gmail.com>
2020-07-19 20:31:18 +1000

    Moved file to .cpp and added intro text

---
+++ JustNumbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	std::cout << "\"Welcome to Letters & Numbers, where the equasions roam free and the thesaurus is not a dinosaur.\"";
+	std::cout << std::endl;
+	std::cout << "*whispers from offstage*";
+	std::cout << std::endl;
+	std::cout << "\"... well then, welcome to Just Numbers, as we are apparently low on stock in the alphabetical department.\"\n";
+	return 0;
+}
